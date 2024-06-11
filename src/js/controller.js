8bce3800e56412a79f8829928592485127b5f166
Abir Chodha <abirchodha1@gmail.com>
2024-06-11 17:49:41 +0530

    Create our own recipe object from data

--- src/js/controller.js
+++ src/js/controller.js
@@ -19,7 +19,21 @@ const showRecipe = async function () {
     );
     const data = await res.json();
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
   } catch (err) {
     alert(err);
   }
 };
+
+showRecipe();
