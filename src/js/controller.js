cd111b81e85dafc2861653d4a71740468155db18
Abir Chodha <abirchodha1@gmail.com>
2024-06-12 00:38:06 +0530

    Render recipe based on id

--- src/js/controller.js
+++ src/js/controller.js
@@ -30,10 +30,12 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+
     // 1) Loading recipe
     renderSpinner(recipeContainer);
     const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      'https://forkify-api.herokuapp.com/api/v2/recipes/${id}'
     );
     const data = await res.json();
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
@@ -156,3 +158,5 @@ const showRecipe = async function () {
 };
 
 showRecipe();
+
+window.addEventListener('hashchange', showRecipe);
