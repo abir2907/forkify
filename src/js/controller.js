169aa67232035bd72083eb77f457bf91571db254
Abir Chodha <abirchodha1@gmail.com>
2024-06-12 00:41:59 +0530

    Add guard clause for id not existing

--- src/js/controller.js
+++ src/js/controller.js
@@ -32,6 +32,8 @@ const showRecipe = async function () {
   try {
     const id = window.location.hash.slice(1);
 
+    if (!id) return;
+
     // 1) Loading recipe
     renderSpinner(recipeContainer);
     const res = await fetch(
