73f30d6765208029aab4652673f4b6489afa84f5
Abir Chodha <abirchodha1@gmail.com>
2024-06-15 02:20:57 +0530

    Add controlSearchResults()

--- src/js/controller.js
+++ src/js/controller.js
@@ -27,8 +27,18 @@ const controlRecipes = async function () {
   }
 };
 
+const controlSearchResults = async function () {
+  try {
+    await model.loadSearchResults('pizza');
+    console.log(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
 };
 
 init();
+controlSearchResults();
