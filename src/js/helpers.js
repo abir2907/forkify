530b20ea76f6adff377975d15c6fe83165723794
Abir Chodha <abirchodha1@gmail.com>
2024-06-15 02:16:12 +0530

    Fix bug

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -11,7 +11,8 @@ const timeout = function (s) {
 
 export const getJSON = async function (url) {
   try {
-    const res = await Promise.race([(fetch(url), timeout(TIMEOUT_SEC))]);
+    fetchRes = fetch(url);
+    const res = await Promise.race([fetchRes, timeout(TIMEOUT_SEC)]);
     const data = await res.json();
 
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
