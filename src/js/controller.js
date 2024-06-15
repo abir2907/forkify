49a2759163afd0e364ad5fd91206ea05d2ab1992
Abir Chodha <abirchodha1@gmail.com>
2024-06-16 01:13:52 +0530

    Create resultsView.js

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,6 +1,7 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 import 'core-js/actual';
 import 'regenerator-runtime/runtime';
@@ -24,10 +25,16 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
+
+    // 1) Get search query
     const query = searchView.getQuery();
     if (!query) return;
 
+    // 2) Load search results
     await model.loadSearchResults(query);
+
+    // 3) Load search results
     console.log(model.state.search.results);
   } catch (err) {
     console.log(err);
