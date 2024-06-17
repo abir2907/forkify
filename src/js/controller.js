550cd63bd89a4dfeab51a6e16e1027193a3d78eb
Abir Chodha <abirchodha1@gmail.com>
2024-06-18 00:23:17 +0530

    Create updateServings() function

--- src/js/controller.js
+++ src/js/controller.js
@@ -57,10 +57,19 @@ const controlPagination = function (goToPage) {
   paginationView.render(model.state.search);
 };
 
+const controlServings = function () {
+  // Update the recipe servings (in state)
+  model.updateServings(8);
+
+  // Update the recipe view
+  recipeView.render(model.state.recipe);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
+  controlServings();
 };
 
 init();
