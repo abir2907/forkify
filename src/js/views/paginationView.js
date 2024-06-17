8d73ab70eeecb11565e850d159d3b0bafca8f06d
Abir Chodha <abirchodha1@gmail.com>
2024-06-17 18:21:35 +0530

    Add guard clause and convert goToPage to a number

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -7,9 +7,9 @@ class PaginationView extends View {
   addHandlerClick(handler) {
     this._parentElement.addEventListener('click', function (e) {
       const btn = e.target.closest('.btn--inline');
-      console.log(btn);
+      if (!btn) return;
 
-      const goToPage = btn.dataset.goto;
+      const goToPage = +btn.dataset.goto;
       console.log(goToPage);
 
       handler();
