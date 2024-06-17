69457956f6c80f59f3ddc525c58b644a6dd1a93a
Abir Chodha <abirchodha1@gmail.com>
2024-06-17 17:54:21 +0530

    Add logic for 1st page, last page, other page, etc

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -5,13 +5,28 @@ class PaginationView extends View {
   _parentElement = document.querySelector('.pagination');
 
   _generateMarkup() {
-    const numPages = this._data.results.length / this._data.resultsPerPage;
+    const numPages = Math.ceil(
+      this._data.results.length / this._data.resultsPerPage
+    );
     console.log(numPages);
 
     // Page 1 and there are other pages
-    // Page 1, and there are NO other pages
+    if (this._data.page === 1 && numPages > 1) {
+      return 'page 1, others';
+    }
+
     // Last page
+    if (this._data.page === numPages && numPages > 1) {
+      return 'last page';
+    }
+
     // Other page
+    if (this._data.page < numPages) {
+      return 'other page';
+    }
+
+    // Page 1, and there are NO other pages
+    return 'only 1 page';
   }
 }
 
