efcae3576a9a5fc7913f2ad5a4b52c2cf000ea75
Abir Chodha <abirchodha1@gmail.com>
2024-06-18 00:57:08 +0530

    Clean code, use destructuring

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -15,8 +15,8 @@ class RecipeView extends View {
     this._parentElement.addEventListener('click', function (e) {
       const btn = e.target.closest('.btn--update-servings');
       if (!btn) return;
-      const updateTo = +btn.dataset.updateTo;
-      if (updateTo > 0) handler(updateTo);
+      const { updateTo } = btn.dataset;
+      if (+updateTo > 0) handler(+updateTo);
     });
   }
   _generateMarkup() {
