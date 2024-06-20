5c86ad2dbb96f0edb2f620cd89feaaf0719634e2
Abir Chodha <abirchodha1@gmail.com>
2024-06-21 00:25:34 +0530

    Change attributes of cur element diff from new el

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -26,11 +26,18 @@ export default class View {
     newElements.forEach((newEl, i) => {
       const curEl = curElements[i];
 
+      // Update changed TEXT
       if (
         !newEl.isEqualNode(curEl) &&
         newEl.firstChild?.nodeValue.trim() !== ''
       )
         curEl.textContent = newEl.textContent;
+
+      // Update changed ATTRIBUTES
+      if (!newEl.isEqualNode(curEl))
+        Array.from(newEl.attributes).forEach(attr =>
+          curEl.setAttribute(attr.name, attr.value)
+        );
     });
   }
 
