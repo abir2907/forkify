79e210138087005084a32ac890ee6cabe797882b
Abir Chodha <abirchodha1@gmail.com>
2024-06-20 22:03:33 +0530

    Compare curEl with newEl using isEqualNode()

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -22,6 +22,11 @@ export default class View {
     const newDOM = document.createRange().createContextualFragment(newMarkup);
     const newElements = Array.from(newDOM.querySelectorAll('*'));
     const curElements = Array.from(this._parentElement.querySelectorAll('*'));
+
+    newElements.forEach((newEl, i) => {
+      const curEl = curElements[i];
+      console.log(curEl, newEl.isEqualNode(curEl));
+    });
   }
 
   _clear() {
