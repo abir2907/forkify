dbdfdfd5744c80b6ebdb7ee6e50a0811e44936b8
Abir Chodha <abirchodha1@gmail.com>
2024-06-26 01:09:28 +0530

    Get quantity, unit, description of each ingredient

--- src/js/model.js
+++ src/js/model.js
@@ -110,8 +110,13 @@ const clearBookmarks = function () {
 };
 
 export const uploadRecipe = async function (newRecipe) {
-  const ingredients = Object.entries(newRecipe).filter(
-    entry => entry[0].startsWith('ingredient') && entry[1] !== ''
-  );
+  const ingredients = Object.entries(newRecipe)
+    .filter(entry => entry[0].startsWith('ingredient') && entry[1] !== '')
+    .map(ing => {
+      const [quantity, unit, description] = ing[1]
+        .replaceAll(' ', '')
+        .split(',');
+      return { quantity, unit, description };
+    });
   console.log(ingredients);
 };
