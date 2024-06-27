0594ce7bc32df7e2650e363c2720d1ef44fced7d
Abir Chodha <abirchodha1@gmail.com>
2024-06-27 22:46:35 +0530

    Send recipe object to API using key

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,4 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = '67b40ae1-9dc2-4526-9372-30c7247f08e0';
