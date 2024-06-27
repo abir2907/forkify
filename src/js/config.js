c6d9d89ca055dcf0e44c60247df2ecf6fd2086ca
Abir Chodha <abirchodha1@gmail.com>
2024-06-27 23:11:14 +0530

    Show success msg and close modal window

--- src/js/config.js
+++ src/js/config.js
@@ -2,3 +2,4 @@ export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const KEY = '67b40ae1-9dc2-4526-9372-30c7247f08e0';
+export const MODAL_CLOSE_SEC = 2.5;
--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,5 @@
 import * as model from './model.js';
+import { MODAL_CLOSE_SEC } from './config.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
@@ -95,6 +96,14 @@ const controlAddRecipe = async function (newRecipe) {
 
     // Render recipe
     recipeView.render(model.state.recipe);
+
+    // Success message
+    addRecipeView.renderMessage();
+
+    // Close form window
+    setTimeout(function () {
+      addRecipeView.toggleWindow();
+    }, MODAL_CLOSE_SEC * 1000);
   } catch (err) {
     console.error('💥', err);
     addRecipeView.renderError(err.message);
