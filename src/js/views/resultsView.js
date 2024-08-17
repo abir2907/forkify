e9aae04d2ebd111cfed2c733c57265e696e90a7b
Abir Chodha <abirchodha1@gmail.com>
2024-08-18 03:25:17 +0530

    Fix error message

--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -4,7 +4,7 @@ import icons from 'url:../../img/icons.svg';
 
 class ResultsView extends View {
   _parentElement = document.querySelector('.results');
-  _errorMessage = 'No recipes found for you query! PLease try again ;)';
+  _errorMessage = 'No recipes found for you query! Please try again ;)';
   _message = '';
 
   _generateMarkup() {
